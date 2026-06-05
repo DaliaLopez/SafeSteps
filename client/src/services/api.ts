ccc4d9a284111c6a2e5346b70cd32c686cc500a2
DaliaLopez <1112046308@u.icesi.edu.co>
2026-06-05 07:50:54 -0500

    add api url to api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:1234/api';
+const API_URL = import.meta.env.VITE_API_URL ?? 'https://safesteps-backend.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
